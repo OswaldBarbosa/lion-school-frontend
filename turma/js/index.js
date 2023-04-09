594417a1b3a7b91afaa604246dacd2b585fdd208
Milleny Cruz <X257389@one.merckgroup.com>
2023-04-09 19:32:45 -0300

    socorro

--- turma/js/index.js
+++ turma/js/index.js
@@ -1,12 +1,12 @@
 'use strict'
 
-import { getAlunos, getAlunosStatus } from "../js/main.js"
+import { getAlunos } from "../js/main.js"
+import { getAlunosStatus } from "../js/main.js"
 
 const nomeCurso = localStorage.getItem('nomeCurso')
 
 const alunosCurso = await getAlunos('curso')
-const alunosCursando = await getAlunosStatus('cursando')
-const alunosFinalizado = await getAlunosStatus('finalizado')
+const alunos = await getAlunosStatus()
 
 const criarTitulo = () => {
     const containerTitulo = document.querySelector('.container-nome-curso')
@@ -28,6 +28,12 @@ const criarCard = (aluno) => {
     cardAluno.classList.add('card-alunos')
     cardAluno.href = '../alunos/index.html'
 
+    if (aluno.status == 'Cursando') {
+        cardAluno.classList.add('card-alunos-cursando')
+    } else {
+        cardAluno.classList.add('card-alunos-finalizado')
+    }
+
     const fotoAluno = document.createElement('img')
     fotoAluno.classList.add('foto-aluno')
     fotoAluno.src = `${aluno.foto}`
@@ -40,7 +46,35 @@ const criarCard = (aluno) => {
     containerCard.append(cardAluno)
     cardAluno.append(fotoAluno, nomeAluno)
 
+    cardAluno.addEventListener('click', () => {
+        localStorage.setItem('matriculaAluno', matricula.textContent)
+    })
+
     return cardAluno
+}
+
+const CursandoEFinalizado = () => {
+    const buttons = document.querySelectorAll('.container-card-alunos');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', async () => {
+
+            const idClicado = button.id;
+
+            if (button.id == "status") {
+                carregarCard()
+            } else {
+                const retorna = await getAlunosStatus(idClicado)
+                const cardJSON = retorna.aluno.map(criarCard);
+                const card = document.getElementById('container-card-alunos')
+
+                card.replaceChildren(...cardJSON)
+            }
+
+        })
+
+    })
+
 }
 
 const voltar = () => {
@@ -51,12 +85,16 @@ const voltar = () => {
 }
 
 const carregarCard = () => {
-    criarTitulo()
     const container = document.getElementById('container-card-alunos')
     const card = alunosCurso.alunos.map(criarCard)
 
     container.append(...card)
+
+    // criarTitulo()
+
 }
+
+CursandoEFinalizado()
 
 carregarCard()
 
